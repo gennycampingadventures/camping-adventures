74d0a1f4b4bbf7823fba62c533942a5ffa7c051f
gennycampingadventures <jay.kaye31+gennycampingadventures@gmail.com>
2017-02-11 18:57:40 -0500

    Refactor items js and html to add and delete items

--- assets/js/items.js
+++ assets/js/items.js
@@ -1,76 +1,129 @@
 $(document).ready(function() {
+
   // Get a reference to the database service
   var database = firebase.database();
-
   var utensilRef = database.ref('utensils');
-  utensilRef.on('value', function(snapshot) {
-      var utensilList = document.getElementById('utensils-list');
-      $(utensilList).empty();
-      snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.val());
-        var utensilData = childSnapshot.val();
-        var newItem = document.createElement('li');
-        newItem.appendChild(document.createTextNode(utensilData));
-        utensilList.appendChild(newItem);
-      });
-  });
-
-
   var foodRef = database.ref('food');
-  foodRef.on('value', function(snapshot) {
-      var foodList = document.getElementById('food-list');
-      $(foodList).empty();
-      snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.val());
-        var foodData = childSnapshot.val();
-        var newItem = document.createElement('li');
-        newItem.appendChild(document.createTextNode(foodData));
-        foodList.appendChild(newItem);
-      });
-  });
-
   var miscRef = database.ref('misc');
-  miscRef.on('value', function(snapshot) {
-      var miscList = document.getElementById('misc-list');
-      $(miscList).empty();
-      snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.val());
-        var miscData = childSnapshot.val();
-        var newItem = document.createElement('li');
-        newItem.appendChild(document.createTextNode(miscData));
-        miscList.appendChild(newItem);
-      });
-  });
 
-  var elements = document.querySelectorAll("#add-item-list-one, #add-item-list-two, #add-item-list-three");
+  function createDeleteButton(context, item, itemType, func) {
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "Delete item";
+    button.onclick = func;
+    $(button).addClass("delete-"+itemType+"-item");
+    $(button).attr("item-value", item);
+    $(context).append(button);
+  }
+
+  var loadItemList = function(itemType) {
+    var databaseRef = database.ref(itemType);
+    var itemList = document.getElementById(itemType + "-list");
+    databaseRef.on('value', function(snapshot) {
+      $(itemList).empty()
+      snapshot.forEach(function(childSnapshot) {
+        var itemData = childSnapshot.val();
+        var itemValue = document.createElement('li');
+        itemValue.appendChild(document.createTextNode(itemData));
+        itemList.appendChild(itemValue);
+        createDeleteButton(itemValue, itemData, itemType);
+      })
+    })
+  }
+
+  // loadFoodList();
+  loadItemList('utensils');
+  loadItemList('food');
+  loadItemList('misc');
+
+  // able to dynamically add buttons, but needs help
+  // need to know how to pass value of li for clicked button in order to grab item value
+  // UPDATE: CURRENTLY we can add a button and then add an attribute with the value of the li item clicked
+  //  NEXT: on button click, find the value,then delete from database
+  //  THEN: remove the button as well
+
+  // I DONT REMEMEBR WHAT THE BELOW IS FOR, IT SEEMS REDUNDANT FROM ABOVE
+  // SOMETHING ABOUT ADDING THINGS DYNAMICALLY
+  // OR RIGHT THIS IS FOR UPDATES / STRIKETHROUGHS
+  // $("#food-list").click(function() {
+  //   var itemValue = this.innerHTML;
+  //   foodRef.on('value', function(snapshot) {
+  //     var foodList = document.getElementById('food-list');
+  //     $(foodList).empty();
+  //     snapshot.forEach(function(childSnapshot) {
+  //       var foodData = childSnapshot.val();
+  //       var newItem = document.createElement('li');
+  //       newItem.appendChild(document.createTextNode(foodData));
+  //       foodList.appendChild(newItem);
+  //       createDeleteButton(newItem)
+  //     });
+  //   });
+  // });
+
+  var elements = document.querySelectorAll("#add-utensil-button, #add-food-button, #add-misc-button");
   for (var i = 0; i < elements.length; i++) {
     elements[i].addEventListener("click", function() {
-      var inputId = this.id;
-      if (inputId=="add-item-list-one") {
-        var itemId = document.getElementById('item-list-one');
-        var itemValue = itemId.value;
-        var newUtensilKey = firebase.database().ref("utensils").push().key;
-        var updates = {};
-        updates[newUtensilKey] = itemValue;
-        itemId.value = "";
-        firebase.database().ref("utensils").update(updates);
-      } else if (inputId=="add-item-list-two") {
-        var itemId = document.getElementById('item-list-two');
-        var itemValue = itemId.value;
-        var newFoodKey = firebase.database().ref("food").push().key;
-        var updates = {};
-        updates[newFoodKey] = itemValue;
-        itemId.value = "";
-        return firebase.database().ref("food").update(updates);
-      } else if (inputId=="add-item-list-three") {
-        var itemId = document.getElementById('item-list-three');
-        var itemValue = itemId.value;
-        var newMiscKey = firebase.database().ref("misc").push().key;
-        var updates = {};
-        updates[newMiscKey] = itemValue;
-        itemId.value = "";
-        return firebase.database().ref("misc").update(updates);
+      var buttonId = this.id;
+      if (buttonId=="add-utensil-button") {
+        var inputId = document.getElementById('add-utensil-input');
+        var itemValue = inputId.value;
+        if (itemValue == "") {
+          alert("Put something in fool");
+        }
+        else {
+          var newUtensilKey = firebase.database().ref("utensils").push().key;
+          var updates = {};
+          updates[newUtensilKey] = itemValue;
+          inputId.value = "";
+          firebase.database().ref("utensils").update(updates);
+        }
+      } else if (buttonId=="add-food-button") {
+        var inputId = document.getElementById('add-food-input');
+        var itemValue = inputId.value;
+        if (itemValue == "") {
+          alert("Put something in fool");
+        } else {
+          var newFoodKey = firebase.database().ref("food").push().key;
+          var updates = {};
+          updates[newFoodKey] = itemValue;
+          inputId.value = "";
+          return firebase.database().ref("food").update(updates);
+        }
+      } else if (buttonId=="add-misc-button") {
+        var inputId = document.getElementById('add-misc-input');
+        var itemValue = inputId.value;
+        if (itemValue == "") {
+
+        } else {
+          var newMiscKey = firebase.database().ref("misc").push().key;
+          var updates = {};
+          updates[newMiscKey] = itemValue;
+          inputId.value = "";
+          return firebase.database().ref("misc").update(updates);
+        }
       }
     });
   }
+
+  $(document).on('click', '.delete-food-item,.delete-utensils-item,.delete-misc-item', function() {
+    var buttonClassName = this.className;
+    if (buttonClassName == "delete-utensils-item") {
+      var databaseRef = firebase.database().ref('utensils');
+    } else if (buttonClassName == "delete-food-item") {
+      var databaseRef = firebase.database().ref('food');
+    } else if (buttonClassName == "delete-misc-item") {
+      var databaseRef = firebase.database().ref('misc');
+    }
+    var item = this.getAttribute('item-value');
+    databaseRef.orderByValue().equalTo(item).on('child_added', function(snapshot) {
+      snapshot.ref.remove();
+    })
+    $(buttonClassName).each(function() {
+      var button = $(this);
+      var parent = button.parent();
+      var itemValue = parent.text();
+      button.attr("item-value", itemValue);
+    })
+  })
+
 })
